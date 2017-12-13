0ca4b2c73701240c332ed61977c8ca71f51affab
omdelafuente <mrooskitar@gmail.com>
2017-12-13 00:10:27 +0000

    Implementada vista de todos los eventos creados

--- GR82.15.eventsMicroservice/src/main/java/es/uc3m/tiw/controller/Controller.java
+++ GR82.15.eventsMicroservice/src/main/java/es/uc3m/tiw/controller/Controller.java
@@ -1,5 +1,7 @@
 package es.uc3m.tiw.controller;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.uc3m.tiw.domains.*;
@@ -19,11 +22,6 @@ public class Controller {
 	@Autowired
 	EventDAO eventDAO;
 	
-	@RequestMapping(value="/event", method=RequestMethod.GET)
-	public List<Event> getAllEvents(){	
-		return eventDAO.findAll();
-	}
-	
 	@RequestMapping(value="/event", method=RequestMethod.POST)
 	public Event createEvent(@RequestBody Event event){
 		return eventDAO.save(event);
@@ -34,9 +32,47 @@ public class Controller {
 		return eventDAO.findById(id);
 	}
 	
+	@RequestMapping(value="/event", method=RequestMethod.GET)
+	public List<Event> getEvents(
+			@RequestParam(value = "str", required = false) String str,
+			@RequestParam(value = "email", required = false) String email,
+			@RequestParam(value = "title", required = false) String title,
+			@RequestParam(value = "category", required = false) String category,
+			@RequestParam(value = "place", required = false) String place,
+			@RequestParam(value = "description", required = false) String description,
+			@RequestParam(value = "state", required = false) String state,
+			@RequestParam(value = "priceMin", required = false) BigDecimal priceMin,
+			@RequestParam(value = "priceMax", required = false) BigDecimal priceMax,
+			@RequestParam(value = "dateMin", required = false) LocalDateTime dateMin,
+			@RequestParam(value = "dateMax", required = false) LocalDateTime dateMax){
+		
+		if(str == null && 
+		email == null && 
+		title == null && 
+		category == null && 
+		place == null && 
+		description == null && 
+		state == null && 
+		priceMin == null && 
+		priceMax == null && 
+		dateMin == null && 
+		dateMin == null){
+			return eventDAO.findAll();
+		}
+		else {
+			
+			if(str != null){
+				return eventDAO.findMatchingString(str);
+			}
+			else if(email != null){
+				return eventDAO.findByCreatorEmail(email);				
+			}
+			else {
+				return eventDAO.findByMultipleFields(title, category, place, description, state, priceMin, priceMax, dateMin, dateMax);
+			}
+			
+		}
+		 
+	}
 	
-	
-	
-	
-
 }
--- GR82.15.eventsMicroservice/src/main/java/es/uc3m/tiw/domains/EventDAO.java
+++ GR82.15.eventsMicroservice/src/main/java/es/uc3m/tiw/domains/EventDAO.java
@@ -1,12 +1,27 @@
 package es.uc3m.tiw.domains;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface EventDAO extends CrudRepository<Event, Integer> {
 	
-	public List<Event> findAll();
 	public Event findById(int id);
+	
+	public List<Event> findAll();
+	
+	@Query("SELECT c FROM Event c WHERE c.creator.email = :email")
+	public List<Event> findByCreatorEmail(@Param("email") String email);
+	
+	@Query("SELECT c FROM Event c WHERE c.title LIKE CONCAT('%',:str,'%') OR c.category LIKE CONCAT('%',:str,'%') OR c.price LIKE CONCAT('%',:str,'%') OR function('date_format',c.eventDate, '%r %M %d %Y') LIKE CONCAT('%',:str,'%') OR c.place LIKE CONCAT('%',:str,'%') OR c.description LIKE CONCAT('%',:str,'%') OR c.state LIKE CONCAT('%',:str,'%')")
+	public List<Event> findMatchingString(@Param("str") String str);
+	
+	@Query("SELECT c FROM Event c WHERE (:title IS NULL OR c.title LIKE CONCAT('%',:title,'%')) AND (:category IS NULL OR c.category = :category) AND (:place IS NULL OR c.place LIKE CONCAT('%',:place,'%')) AND (:description IS NULL OR c.description LIKE CONCAT('%',:description,'%')) AND (:state IS NULL OR c.state = :state) AND (:priceMin IS NULL OR c.price >= :priceMin) AND (:priceMax IS NULL OR c.price <= :priceMax) AND (:dateMin IS NULL OR c.eventDate >= :dateMin) AND (:dateMax IS NULL OR c.eventDate <= :dateMax)")
+	public List<Event> findByMultipleFields(@Param("title") String title, @Param("category") String category, @Param("place") String place, @Param("description") String description, @Param("state") String state, @Param("priceMin") BigDecimal priceMin, @Param("priceMax") BigDecimal priceMax, @Param("dateMin") LocalDateTime dateMin, @Param("dateMax") LocalDateTime dateMax);
+	
 	
 }
